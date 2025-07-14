14e45ae06b7b90550bf0c9b04932af610a625c8e
Lokman Hossain <lokmanhossain@Lokmans-MacBook-Air.local>
2025-07-15 00:15:33 +0600

    update9

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://maac-blog.onrender.com";
 
 interface ApiResponse<T> {
   data?: T;
